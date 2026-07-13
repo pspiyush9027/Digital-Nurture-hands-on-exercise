d4e77b913ff57267abf5cf69f71d41a1742c7e55
Piyush <piyushagrawa9027@gmail.com>
2026-07-13 10:54:42 +0530

    [piyush] .add -spring maven application class-

---
+++ Spring_Core_Maven/Creating-Configuring-Maven-Project/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,8 @@
+package com.library;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        System.out.println("LibraryManagement Maven project is configured with Spring dependencies.");
+    }
+}
