3a6728648d0691fb6a858af0ee64517cf2c0702b
Piyush <piyushagrawa9027@gmail.com>
2026-07-16 09:59:45 +0530

    [piyush] .add -hibernate vs spring data jpa foundation-

---
+++ Week 2/Spring_Data-JPA/Hibernate_vs_Spring_Data_JPA/src/main/java/com/library/HibernateVsSpringDataJpaApplication.java
@@ -0,0 +1,8 @@
+package com.library;
+
+public class HibernateVsSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hibernate vs Spring Data JPA comparison project is ready.");
+    }
+}
