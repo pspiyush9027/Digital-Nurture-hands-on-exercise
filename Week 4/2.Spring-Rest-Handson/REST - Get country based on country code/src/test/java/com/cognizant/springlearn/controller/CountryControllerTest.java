c2fc33cf427eae671969bb1a37986b947ea53a5b
Piyush <piyushagrawa9027@gmail.com>
2026-07-22 18:42:32 +0530

    [piyush] .add -country by code test and output-

---
+++ Week 4/2.Spring-Rest-Handson/REST - Get country based on country code/src/test/java/com/cognizant/springlearn/controller/CountryControllerTest.java
@@ -0,0 +1,38 @@
+package com.cognizant.springlearn.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CountryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldFindIndiaForLowercaseCountryCode() throws Exception {
+        mockMvc.perform(get("/country/in"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.code").value("IN"))
+                .andExpect(jsonPath("$.name").value("India"));
+    }
+
+    @Test
+    void shouldFindJapanForUppercaseCountryCode() throws Exception {
+        mockMvc.perform(get("/countries/JP"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("JP"))
+                .andExpect(jsonPath("$.name").value("Japan"));
+    }
+}
