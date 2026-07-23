8cfab22096c519bd20b074df1a0564f23b197204
Piyush <piyushagrawa9027@gmail.com>
2026-07-23 10:08:35 +0530

    [piyush] .add -react welcome page-

---
+++ Week 5/React/ReactJs-HOL/myfirstreact/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="welcome-card">
+      <p className="eyebrow">MY FIRST REACT APP</p>
+      <h1>welcome to the first session of React</h1>
+    </main>
+  );
+}
+
+export default App;
